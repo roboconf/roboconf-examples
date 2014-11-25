c4d95fe1a0e9b587e436e51d1c60da8da12c0a12
Pierre-Yves GIBELLO <pygibello@linagora.com>
2014-11-25 14:15:15 +0100

    Apache config file name was wrong for most installation.

--- apache-tomcat-bash/graph/Apache/scripts/deploy.sh
+++ apache-tomcat-bash/graph/Apache/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yes | apt-get install apache2 libapache2-mod-jk
-cp $ROBOCONF_FILES_DIR/default /etc/apache2/sites-available/
+cp $ROBOCONF_FILES_DIR/000-default.conf /etc/apache2/sites-available/
 
 apache2ctl stop
 
